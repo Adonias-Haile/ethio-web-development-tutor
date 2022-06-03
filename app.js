132ba3f31c1174f736a1d77d262c6ab24da9fbcc
Adonias-Haile <adoniashaile1@gamil.com>
2022-06-03 21:35:52 +0300

    version 1

---
+++ app.js
@@ -0,0 +1,6 @@
+function testFunction(){       
+ alert("Name:" + " Adonias Haile" +" "+ "ID: 1200818"+" ||"+"  "+
+"Name:" + " Ademe checklie" +" "+ "ID: 1200808"+"  "+"||"+"  "+
+"Name:" + " Beti Girm" +" "+ "ID: 1201165"+" ,"); 
+      
+} 
